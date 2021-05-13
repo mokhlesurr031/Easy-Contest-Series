7767c5f5b5393a8c9537ab532dd4ee91b18fb5bf
JeffyMahin <Mahin1996>
2021-05-14 05:33:41 +0600

    problem solve and data structure practice

--- DS and Algo/linked_list_edureka.py
+++ DS and Algo/linked_list_edureka.py
@@ -1,2 +1,9 @@
 class Node:
-    self.data = data
+    def __init__(self, data):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = head
+
